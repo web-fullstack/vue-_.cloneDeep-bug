25b637e5d003e566f033f75ad6b4cbe422e6991b
xgqfrms <xgqfrms@outlook.com>
2021-04-14 01:32:36 +0800

    js deep nested object generator ???

--- deep-clone-bug/src/App.vue
+++ deep-clone-bug/src/App.vue
@@ -20,9 +20,9 @@ export default {
       console.log('to', to);
       console.log('from', from);
       if (from.path.includes('/')) {
-        // this.$oldRoute = _.cloneDeep(from);
+        this.$oldRoute = _.cloneDeep(from);
         // this.$oldRoute = JSON.parse(JSON.stringify(from));
-        this.$oldRoute = from;
+        // this.$oldRoute = from;
         console.log('from', from);
         console.log('this.$oldRoute', this.$oldRoute);
         // params: {}
